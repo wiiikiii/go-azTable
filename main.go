f9e8f8d350c27fcbb6049e90d7770c4d1a00457b
seb07-cloud <wild.sebastian@outlook.de>
2022-11-15 07:09:40 +0100

    initial

--- main.go
+++ main.go
@@ -2,54 +2,70 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
+	"os"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/data/aztables"
 )
 
-var storageaccountkey string = "OVCT1/fNFiMJSaFX38SEJT/kevTkyWLph5gZHRMre9ilguo52h3WlRrwpkc+SF/eC7sNPuZ0MJxu+AStayyzuQ=="
+type InventoryEntity struct {
+	aztables.Entity
+	Price       float32
+	Inventory   int32
+	ProductName string
+	OnSale      bool
+}
+
+type PurchasedEntity struct {
+	aztables.Entity
+	Price float32
+	ProductName string
+	OnSale bool
+}
+
+func getClient() *aztables.Client {
+	accountName, ok := os.LookupEnv("AZURE_STORAGE_ACCOUNT")
+	if !ok {
+		panic("AZURE_STORAGE_ACCOUNT environment variable not found")
+	}
+
+	tableName, ok := os.LookupEnv("AZURE_TABLE_NAME")
+	if !ok {
+		panic("AZURE_TABLE_NAME environment variable not found")
+	}
+
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		panic(err)
+	}
+	serviceURL := fmt.Sprintf("https://%s.table.core.windows.net/%s", accountName, tableName)
+	client, err := aztables.NewClient(serviceURL, cred, nil)
+	if err != nil {
+		panic(err)
+	}
+	return client
+}
+
+func listEntities(client *aztables.Client) {
+	listPager := client.GetEntity("AVD", "Input" )
+	pageCount := 0
+	for listPager.More() {
+		response, err := listPager.NextPage(context.TODO())
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("There are %d entities in page #%d\n", len(response.Entities), pageCount)
+		pageCount += 1
+	}
+}
 
 func main() {
 
-	accountname := "axiansvdconfig"
-	serviceURL := accountname + ".table.core.windows.net"
+	fmt.Println("Authenticating...")
+	client := getClient()
 
-    cred, err := aztables.NewSharedKeyCredential(serviceURL, storageaccountkey)
-    if err != nil {
-        panic(err)
-    }
-    client, err := aztables.NewServiceClientWithSharedKey(serviceURL, cred, nil)
-    if err != nil {
-        panic(err)
-    }
+	fmt.Println("Calculating all entities in the table...")
+	listEntities(client)
 
-	filter := "PartitionKey eq 'AVD' or RowKey eq 'Input'"
-    options := &aztables.ListEntitiesOptions{
-        Filter: &filter,
-        Select: to.Ptr("RowKey,Value,Product,Available"),
-        Top: to.Ptr(int32(15)),
-    }
-
-    pager := client.NewListEntitiesPager(options)
-    pageCount := 0
-    for pager.More() {
-        response, err := pager.NextPage(context.TODO())
-        if err != nil {
-            panic(err)
-        }
-        fmt.Printf("There are %d entities in page #%d\n", len(response.Entities), pageCount)
-        pageCount += 1
-
-        for _, entity := range response.Entities {
-            var myEntity aztables.EDMEntity
-            err = json.Unmarshal(entity, &myEntity)
-            if err != nil {
-                panic(err)
-            }
-
-            fmt.Printf("Received: %v, %v, %v, %v\n", myEntity.RowKey, myEntity.Properties["Value"], myEntity.Properties["Product"], myEntity.Properties["Available"])
-        }
-    }
 }
