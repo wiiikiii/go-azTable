8f59d69bf0cf9b7897c94f99d42dee6ae6d97fdb
seb07-cloud <wild.sebastian@outlook.de>
2022-11-18 09:30:30 +0100

    changed folder structure

--- go.mod
+++ go.mod
@@ -1,25 +1,15 @@
-module go-webserver-admin
+module go-table
 
 go 1.19
 
 require (
 	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.2.0 // indirect
-	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.2.0 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/data/aztables v1.0.1 // indirect
-	github.com/Azure/azure-sdk-for-go/sdk/internal v1.1.1 // indirect
-	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources v1.0.0 // indirect
-	github.com/AzureAD/microsoft-authentication-library-for-go v0.7.0 // indirect
-	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.0.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/kylelemons/godebug v1.1.0 // indirect
-	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
-	golang.org/x/crypto v0.2.0 // indirect
-	golang.org/x/net v0.2.0 // indirect
-	golang.org/x/sys v0.2.0 // indirect
-	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ pkg/getTableData.go
@@ -0,0 +1,88 @@
+package getTableData
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/data/aztables"
+)
+
+func GetTableData(partitionKey string, rowKey string, tableName string) string {
+
+	type ListEntitiesOptions struct {
+		Filter           *string
+		Select           *string
+		Top              int32
+		NextPartitionKey *string
+		NextRowKey       *string
+	}
+
+	accountName, ok := os.LookupEnv("TABLES_STORAGE_ACCOUNT_NAME")
+	if !ok {
+		panic(" TABLES_STORAGE_ACCOUNT_NAME could not be found")
+	}
+	accountKey, ok := os.LookupEnv("TABLES_PRIMARY_STORAGE_ACCOUNT_KEY")
+	if !ok {
+		panic(" TABLES_PRIMARY_STORAGE_ACCOUNT_KEY could not be found")
+	}
+
+	serviceURL := fmt.Sprintf("https://%s.table.core.windows.net/%s", accountName, tableName)
+
+	cred, err := aztables.NewSharedKeyCredential(accountName, accountKey)
+	if err != nil {
+		panic(err)
+	}
+	client, err := aztables.NewClientWithSharedKey(serviceURL, cred, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	filter := fmt.Sprintf("PartitionKey eq '%s' or RowKey eq '%s'", partitionKey, rowKey)
+	options := &aztables.ListEntitiesOptions{
+		Filter: &filter,
+		Top:    to.Ptr(int32(15)),
+	}
+
+	pager := client.NewListEntitiesPager(options)
+	pageCount := 0
+
+	var Export string
+
+	for pager.More() {
+		response, err := pager.NextPage(context.TODO())
+		if err != nil {
+			panic(err)
+		}
+		pageCount += 1
+
+		for _, entity := range response.Entities {
+			var myEntity aztables.EDMEntity
+			err = json.Unmarshal(entity, &myEntity)
+			if err != nil {
+				panic(err)
+			}
+
+			jsonStr, err := json.Marshal(myEntity.Properties)
+			if err != nil {
+				fmt.Printf("Error: %s", err.Error())
+			} else {
+				fmt.Println(string(jsonStr))
+			}
+
+			err = ioutil.WriteFile("data.json", jsonStr, 0644)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			Export := fmt.Sprintln(string(jsonStr))
+			return Export
+		}
+		return Export
+	}
+	return Export
+}
